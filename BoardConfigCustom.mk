afcc2b9dfa060efbbf4c3e9425e9733c25e218cb
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-23 22:32:25 +0530

    Build vendor_dlkm
    Change-Id: Ic2a0e4cdfdcc448a73bef41e0d2140e9d0aefcf0

--- BoardConfigCustom.mk
+++ BoardConfigCustom.mk
@@ -3,3 +3,11 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+# Partitions
+AB_OTA_PARTITIONS += \
+    vendor_dlkm
+
+BOARD_USES_VENDOR_DLKMIMAGE := true
+BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
