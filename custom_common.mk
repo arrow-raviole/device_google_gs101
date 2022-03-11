c981c2e3288c192aa877cf587caaea3127046635
Chirayu Desai <chirayudesai1@gmail.com>
2022-03-12 04:11:07 +0530

    Build EuiccSupportPixelOverlay
    Change-Id: I46db4122b89798457338f6751982865571529ba6

---
+++ custom_common.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2022 The Custom AOSP Project
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# EUICC
+PRODUCT_PACKAGES += \
+    EuiccSupportPixelOverlay
